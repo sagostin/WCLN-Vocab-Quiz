a2a2a044f605b8d380759beac4cd7b625a3e5f72
Shaun Agostinho <shaunagostinho@gmail.com>
2020-02-05 21:36:52 -0800

    word auto sizing and random shuffle for words & definitions

--- js/game.js
+++ js/game.js
@@ -28,6 +28,22 @@ let json = {
         {
             word: "apple",
             definition: "like a tree potato"
+        },
+        {
+            word: "grape",
+            definition: "lil ting that grows on a vine"
+        },
+        {
+            word: "minecraft",
+            definition: "lego but virtual"
+        },
+        {
+            word: "cisco",
+            definition: "large company that makes the internet run"
+        },
+        {
+            word: "target",
+            definition: "used for playing the game called darts bloop bleep blah glah glewe"
         }
     ]
 };
@@ -199,9 +215,50 @@ let length = 300;
 let sideGap = 20;
 let height = 55;
 let verticalGap = 20;
-let boxCount = 6;
+let boxCount = 6; // 6 fits perfectly so this will be the actual maximum
+
+let vocabList = [];
+let definitionsList = [];
+
+function shuffleLists() {
+    for (let i = 0; i < json.vocabulary.length; i++) {
+        vocabList.push(json.vocabulary[i].word);
+        definitionsList.push(json.vocabulary[i].definition);
+    }
+
+    vocabList = shuffle(vocabList);
+    definitionsList = shuffle(definitionsList);
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/**
+ * Draw the vocab boxes
+ *
+ */
 
 function drawBoxes() {
+
+    if (json.vocabulary.length < 6) {
+        boxCount = json.vocabulary.length;
+    }
 
     //vocab boxes
     for (let i = 0; i < boxCount; i++) {
@@ -232,12 +289,89 @@ function drawBoxes() {
             clickDefinitionBox(event);
         });
     }
+
+    drawBoxText();
+}
+
+let vocabBoxText = [];
+let definitionBoxText = [];
+
+function drawBoxText() {
+    shuffleLists();
+
+    //vocab boxes
+    for (let i = 0; i < boxCount; i++) {
+
+        let textSize = 24;
+        let fakeTextForSizing = new createjs.Text(vocabList[i], textSize + "px Comic Sans MS", "#FFFFFF");
+        while (fakeTextForSizing.getMeasuredWidth() >= length - sideGap * 2) {
+            textSize -= 1;
+            fakeTextForSizing = new createjs.Text(vocabList[i], textSize + "px Comic Sans MS", "#FFFFFF");
+        }
+
+        vocabBoxText[i] = new createjs.Text(vocabList[i], textSize + "px Comic Sans MS", "#000");
+        vocabBoxText[i].textBaseline = "alphabetic";
+        vocabBoxText[i].textAlign = 'center';
+        vocabBoxText[i].x = vocabBoxes[i].graphics.command.x + length / 2;
+        vocabBoxText[i].y = vocabBoxes[i].graphics.command.y + height / 2 + vocabBoxText[i].getMeasuredHeight() / 4;
+
+        stage.addChild(vocabBoxText[i]);
+    }
+
+    //definition boxes
+    for (let i = 0; i < boxCount; i++) {
+
+        let newDefinitionText = "";
+        let splitLength = definitionsList[i].split(" ").length;
+        console.log(splitLength);
+        for (let def = 0; def < splitLength; def++) {
+            if (def !== parseInt(definitionsList[i].split(" ").length / 2)) {
+                newDefinitionText += definitionsList[i].split(" ")[def] + " ";
+            } else {
+                newDefinitionText += definitionsList[i].split(" ")[def] + "\n";
+            }
+        }
+
+        let textSize = 22;
+        let fakeTextForSizing = new createjs.Text(
+            newDefinitionText != "" ? newDefinitionText :
+                definitionsList[i], textSize + "px Comic Sans MS", "#FFFFFF");
+        while (fakeTextForSizing.getMeasuredWidth() >= length + sideGap * 6.5) {
+            textSize -= 1;
+            fakeTextForSizing = new createjs.Text(
+                newDefinitionText != "" ? newDefinitionText :
+                    definitionsList[i], textSize + "px Comic Sans MS", "#FFFFFF");
+        }
+
+        console.log(newDefinitionText);
+
+        definitionBoxText[i] = new createjs.Text(
+            newDefinitionText != "" ? newDefinitionText :
+                definitionsList[i], textSize + "px Comic Sans MS", "#000");
+        definitionBoxText[i].textBaseline = "alphabetic";
+        definitionBoxText[i].textAlign = 'center';
+        definitionBoxText[i].x = definitionBoxes[i].graphics.command.x + length / 2;
+        definitionBoxText[i].y = definitionBoxes[i].graphics.command.y + height / 2
+            + (newDefinitionText != "" ? -definitionBoxText[i].getMeasuredHeight() / 8 : definitionBoxText[i].getMeasuredHeight() / 4);
+
+        stage.addChild(definitionBoxText[i]);
+    }
 }
 
 let matchedBoxes = {
     matched:
         []
 };
+
+/**
+ * Check if a vocab and definition already are matched
+ * To check one of them, set the other as null.
+ * Only once can be checked, not compared.
+ *
+ * @param vocab
+ * @param definition
+ * @returns {boolean}
+ */
 
 function checkIfAlreadyMatched(vocab, definition) {
     if (vocab != null) {
@@ -257,6 +391,24 @@ function checkIfAlreadyMatched(vocab, definition) {
     }
 }
 
+/**
+ * Match Box Outline Colors
+ *
+ * @type {*[]}
+ */
+
+let matchColorsSettings = [
+    "#c56cf0", "#ffb8b8", "#e1b12c", "#ff9f1a",
+    "#fff200", "#7158e2", "#17c0eb", "#e84393",
+    "#ff3838", "#67e6dc"];
+
+/**
+ * Vocab & Definition Box Number to make the match
+ *
+ * @param vocab
+ * @param definition
+ */
+
 function matchBoxes(vocab, definition) {
     if (!checkIfAlreadyMatched(vocab, null) && !checkIfAlreadyMatched(null, definition)) {
         matchedBoxes.matched.push({vocab: vocab, definition: definition});
@@ -264,27 +416,60 @@ function matchBoxes(vocab, definition) {
         clickedDefinitionBox = undefined;
         clickedVocabBox = undefined;
 
-        matchColor = "#FF0000";
         vocabBoxes[vocab].graphics.setStrokeStyle(4);
-        vocabBoxes[vocab].graphics.beginStroke(matchColor);
+        vocabBoxes[vocab].graphics.beginStroke(matchColorsSettings[(matchedBoxes.matched.length - 1)]);
 
         vocabBoxes[vocab].graphics.beginFill("#FFF").drawRoundRectComplex(
             sideGap, 100 + (verticalGap * vocab) + (vocab * height), length, height,
             10, 10, 10, 10);
 
         definitionBoxes[definition].graphics.setStrokeStyle(4);
-        definitionBoxes[definition].graphics.beginStroke(matchColor);
+        definitionBoxes[definition].graphics.beginStroke(matchColorsSettings[(matchedBoxes.matched.length - 1)]);
 
         definitionBoxes[definition].graphics.beginFill("#FFF").drawRoundRectComplex(
             STAGE_WIDTH - (length + sideGap),
             100 + (verticalGap * definition) + (definition * height), length, height,
             10, 10, 10, 10);
+
+        vocabDefinitionMatchLine(vocab, definition);
     } else {
         console.log("Oh no, an error has been spotted in the wild!")
     }
 }
 
+let vocabDefinitionLines = [];
+
+/**
+ * Draw the line between matched boxes
+ *
+ * @param vocab
+ * @param definition
+ */
+
+function vocabDefinitionMatchLine(vocab, definition) {
+
+    let vpoint = new createjs.Point(vocabBoxes[vocab].graphics.command.x, vocabBoxes[vocab].graphics.command.y);
+    let dpoint = new createjs.Point(definitionBoxes[definition].graphics.command.x, definitionBoxes[definition].graphics.command.y);
+
+    let shape = new createjs.Shape();
+    let line = stage.addChild(shape);
+    line.graphics.beginStroke(matchColorsSettings[(matchedBoxes.matched.length - 1)])
+        .setStrokeStyle(4).moveTo(vpoint.x + length, vpoint.y + (height / 2));
+    let cmd = line.graphics.lineTo(vpoint.x + length, vpoint.y + (height / 2)).command;
+
+    createjs.Tween.get(cmd, {loop: false}).to({x: dpoint.x, y: dpoint.y + (height / 2)}, 750);
+
+    vocabDefinitionLines.push(shape);
+}
+
+
 let clickedVocabBox;
+
+/**
+ * Click a vocab box
+ *
+ * @param event
+ */
 
 function clickVocabBox(event) {
     for (let i = 0; i < vocabBoxes.length; i++) {
@@ -318,6 +503,12 @@ function clickVocabBox(event) {
     }
 }
 
+/**
+ * Unclick a matched vocab box
+ *
+ * @param i
+ */
+
 function unClickVocabBox(i) {
     vocabBoxes[i].graphics.setStrokeStyle(0);
     vocabBoxes[i].graphics.beginStroke("#FFF");
@@ -328,6 +519,12 @@ function unClickVocabBox(i) {
 }
 
 let clickedDefinitionBox;
+
+/**
+ * Click a definition box
+ *
+ * @param event
+ */
 
 function clickDefinitionBox(event) {
     for (let i = 0; i < definitionBoxes.length; i++) {
@@ -361,6 +558,12 @@ function clickDefinitionBox(event) {
         }
     }
 }
+
+/**
+ * Unclick a definition box
+ *
+ * @param i
+ */
 
 function unClickDefinitionBox(i) {
     definitionBoxes[i].graphics.setStrokeStyle(0);
