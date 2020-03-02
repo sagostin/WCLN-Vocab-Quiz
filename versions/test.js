25c34f9f6e4a714f1719d6bffbc4a9666cc0de57
Shaun Agostinho <shaunagostinho@gmail.com>
2020-03-01 20:38:43 -0800

    added versioning system

---
+++ versions/test.js
@@ -0,0 +1,53 @@
+//vocabulary config
+let json = {
+    vocabulary: [
+        {
+            word: "potato",
+            definition: "ground thing like apple but ground"
+        },
+        {
+            word: "apple",
+            definition: "like a tree potato"
+        },
+        {
+            word: "grape",
+            definition: "lil ting that grows on a vine"
+        },
+        {
+            word: "minecraft",
+            definition: "lego but virtual"
+        },
+        {
+            word: "cisco",
+            definition: "large company that makes the internet run"
+        },
+        {
+            word: "target",
+            definition: "used for playing the game called darts bloop bleep blah glah glewe"
+        },
+        {
+            word: "potato1",
+            definition: "ground thing like apple but ground1"
+        },
+        {
+            word: "apple1",
+            definition: "like a tree potato1"
+        },
+        {
+            word: "grape1",
+            definition: "lil ting that grows on a vine1"
+        }/*,
+        {
+            word: "minecraft1",
+            definition: "lego but virtual1"
+        },
+        {
+            word: "cisco1",
+            definition: "large company that makes the internet run1"
+        },
+        {
+            word: "target1",
+            definition: "used for playing the game called darts bloop bleep blah glah glewe1"
+        }*/
+    ]
+};
